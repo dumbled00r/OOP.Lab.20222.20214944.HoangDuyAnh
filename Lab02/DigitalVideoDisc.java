f7bdcda1540760e9169628f8b33cd50edc25a6b6
Hoàng Duy Anh <dumbled00r@dumbled00r.local>
2023-04-28 09:22:13 +0700

    Exercise

---
+++ Lab02/DigitalVideoDisc.java
@@ -0,0 +1,68 @@
+
+public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String director;
+	private int length;
+	private float cost;
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the category
+	 */
+	public String getCategory() {
+		return category;
+	}
+	/**
+	 * @param category the category to set
+	 */
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	/**
+	 * @return the director
+	 */
+	public String getDirector() {
+		return director;
+	}
+	/**
+	 * @param director the director to set
+	 */
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	/**
+	 * @return the length
+	 */
+	public int getLength() {
+		return length;
+	}
+	/**
+	 * @param length the length to set
+	 */
+	public void setLength(int length) {
+		this.length = length;
+	}
+	/**
+	 * @return the cost
+	 */
+	public float getCost() {
+		return cost;
+	}
+	/**
+	 * @param cost the cost to set
+	 */
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+}
