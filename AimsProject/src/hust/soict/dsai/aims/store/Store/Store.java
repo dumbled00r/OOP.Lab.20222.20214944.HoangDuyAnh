de9f29a7dc3a2f69b6103748187fd8861d535ff8
dumbled00r <kazuu.promotion@gmail.com>
2023-05-26 17:06:43 +0700

    Reorganize code

--- AimsProject/src/hust/soict/dsai/aims/store/Store/Store.java
+++ AimsProject/src/hust/soict/dsai/aims/store/Store/Store.java
@@ -6,18 +6,14 @@ import hust.soict.dsai.aims.media.DigitalVideoDisc;
 public class Store {
 	public static final int MAX_NUMBER_OF_DVDS = 100000;
 	private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_NUMBER_OF_DVDS];
-	
 	private int currentNBDVDs;
 	
 	// Add DVD to the score
 	public void addDVD(DigitalVideoDisc disc) {
-		
 		if (currentNBDVDs < MAX_NUMBER_OF_DVDS) {
 			itemsInStore[currentNBDVDs++] = disc;
 			System.out.println("The DVD with title "+ disc.getTitle() + " has been added to the store");
-			
 		}
-		
 		else {
 			System.out.println("The store is currently full");
 		}
@@ -26,7 +22,6 @@ public class Store {
 	// Remove DVD from the store
 	public void removeDVD(DigitalVideoDisc disc) {
 		boolean match = false;
-		
 		// Search for the DVD
 		for (int i = 0; i < currentNBDVDs; i++) {
 			if (itemsInStore[i] == disc) {
@@ -44,9 +39,7 @@ public class Store {
 		else System.out.println("DVD is not found");
 	}
 	
-	
 	//  This is just for testing methods above
-	
 	public void print() {
 		int idx = 1;
 		for (DigitalVideoDisc disc: itemsInStore) {
