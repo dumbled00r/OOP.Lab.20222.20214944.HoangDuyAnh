fc5c183a0e64bf0ca6ff255c83dd4b54362a05b3
dumbled00r <kazuu.promotion@gmail.com>
2023-05-26 17:23:05 +0700

    Re-organize code

--- AimsProject/src/hust/soict/dsai/test/store/StoreTest/StoreTest.java
+++ AimsProject/src/hust/soict/dsai/test/store/StoreTest/StoreTest.java
@@ -1,4 +1,5 @@
 package hust.soict.dsai.test.store.StoreTest;
+
 import hust.soict.dsai.aims.store.Store.*;
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
 
@@ -10,12 +11,10 @@ public class StoreTest {
 				"Animation", "Roger Allers", 87, 19.95f);
 		// Add dvd to store
 		store.addDVD(dvd1);
-		
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
 				"Science Fiction", "George Lucas", 87, 24.95f);
 		// add dvd to store
 		store.addDVD(dvd2);
-		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Animation",
 				"Aladin", 18.99f);
 		// add dvd to store
