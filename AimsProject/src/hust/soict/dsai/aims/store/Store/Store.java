bf9fb38c2f422ecebedac1fdbe70d65c8ba5dd0a
dumbled00r <kazuu.promotion@gmail.com>
2023-05-26 17:38:32 +0700

    Update Store to work with Medias

--- AimsProject/src/hust/soict/dsai/aims/store/Store/Store.java
+++ AimsProject/src/hust/soict/dsai/aims/store/Store/Store.java
@@ -1,54 +1,36 @@
 package hust.soict.dsai.aims.store.Store;
 
-import java.lang.NullPointerException;
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.*;
+import java.util.*;
 
 public class Store {
 	public static final int MAX_NUMBER_OF_DVDS = 100000;
-	private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[MAX_NUMBER_OF_DVDS];
+	private ArrayList<Media> itemsInStore = new ArrayList<Media>();
 	private int currentNBDVDs;
 	
-	// Add DVD to the score
-	public void addDVD(DigitalVideoDisc disc) {
+	// Add Media to the score
+	public void addMedia(Media media) {
 		if (currentNBDVDs < MAX_NUMBER_OF_DVDS) {
-			itemsInStore[currentNBDVDs++] = disc;
-			System.out.println("The DVD with title "+ disc.getTitle() + " has been added to the store");
+			itemsInStore.add(media);
+			System.out.println("The media has been added to the store");
 		}
 		else {
 			System.out.println("The store is currently full");
 		}
 	}
 	
-	// Remove DVD from the store
-	public void removeDVD(DigitalVideoDisc disc) {
-		boolean match = false;
-		// Search for the DVD
-		for (int i = 0; i < currentNBDVDs; i++) {
-			if (itemsInStore[i] == disc) {
-				match = true;
-				for (int j = i; j< currentNBDVDs; j++) {
-					itemsInStore[j] = itemsInStore[j+1];
-				}
-			currentNBDVDs--;
-			break;
-			}
+	// Remove Media from the store
+	public void removeMedia(Media media) {
+		// Check if store is empty
+		if (itemsInStore.size() == 0) {
+			System.out.println("The store is empty");
 		}
-		if (match) {
-			System.out.println("The DVD with title " + disc.getTitle() + " is found and is removed from the store");
+		
+		// Check if item exists in store
+		if (itemsInStore.contains(media)) {
+			itemsInStore.remove(media);
+			System.out.println("The media is found and is removed from the store");
 		}
 		else System.out.println("DVD is not found");
-	}
-	
-	//  This is just for testing methods above
-	public void print() {
-		int idx = 1;
-		for (DigitalVideoDisc disc: itemsInStore) {
-			try {
-				System.out.println(idx + ". " + disc.toString());
-				idx++;
-			}
-			catch (NullPointerException e) {
-			}
-		}
 	}
 }
