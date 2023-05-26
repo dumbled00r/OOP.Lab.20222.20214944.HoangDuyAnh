2b7847160ccccaf8661aa706d205f9e180c828b7
dumbled00r <kazuu.promotion@gmail.com>
2023-05-26 17:21:16 +0700

    Test Media with Cart

--- AimsProject/src/hust/soict/dsai/test/cart/CartTest/CartTest.java
+++ AimsProject/src/hust/soict/dsai/test/cart/CartTest/CartTest.java
@@ -1,32 +1,35 @@
 package hust.soict.dsai.test.cart.CartTest;
+
 import hust.soict.dsai.aims.cart.Cart.*;
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.*;
+
 public class CartTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Create a new cart
 		Cart cart = new Cart();
-		// Create new dvd objects and add them to the cart
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+		// Create new dvd and add it to the cart
+		DigitalVideoDisc dvd1 = 
+				new DigitalVideoDisc("The Lion King",
 				"Animation", "Roger Allers", 87, 19.95f);
-		// add dvd to the cart
-		cart.addDigitalVideoDisc(dvd1);
-		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
-				"Science Fiction", "George Lucas", 87, 24.95f);
-		// add dvd to the cart
-		cart.addDigitalVideoDisc(dvd2);
-		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Animation",
-				"Aladin", 18.99f);
-		// add 2 same dvd to the cart to test
-		cart.addDigitalVideoDisc(dvd3);
-		cart.addDigitalVideoDisc(dvd3);
+		cart.addMedia(dvd1);
+		// Create new book and add it to the cart
+		Book book1 = 
+				new Book("Star Wars", 
+				 "George Lucas", 24.95f);
+		cart.addMedia(book1);
+		// Create new CD and add it to the cart
+		CompactDisc cd1 = 
+				new CompactDisc("Making my way", "Pop",
+				"Unknown", 5,  18.99f, "Son Tung M-TP");
+		cart.addMedia(cd1);
 		// print out cart
 		cart.print();
 		// cart's total cost
 		cart.totalCost();
 		// search for item by ID
-		cart.searchForItem(1);
+		cart.searchForItemByID(1);
 		// search for item by title
-		cart.searchForItem("The Lion King");
+		cart.searchForItemByTitle("The Lion King");
 	}
 }
