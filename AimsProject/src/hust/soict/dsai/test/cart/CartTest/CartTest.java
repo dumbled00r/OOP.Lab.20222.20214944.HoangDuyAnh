72cb110690ac26a796fcd8457720198287c5ee6c
dumbled00r <kazuu.promotion@gmail.com>
2023-05-26 15:17:34 +0700

    Update tests

--- AimsProject/src/hust/soict/dsai/test/cart/CartTest/CartTest.java
+++ AimsProject/src/hust/soict/dsai/test/cart/CartTest/CartTest.java
@@ -1,18 +1,13 @@
 package hust.soict.dsai.test.cart.CartTest;
-
 import hust.soict.dsai.aims.cart.Cart.*;
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
 public class CartTest {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
 		// Create a new cart
 		Cart cart = new Cart();
 		
 		// Create new dvd objects and add them to the cart
-		
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
 				"Animation", "Roger Allers", 87, 19.95f);
 		
@@ -27,12 +22,9 @@ public class CartTest {
 		
 		cart.addDigitalVideoDisc(dvd3);
 		cart.addDigitalVideoDisc(dvd3);
-		
-		
-		
-		
 		cart.print();
-		cart.searchForItem("The Lion King");
+		cart.totalCost();
+		cart.searchForItem(1);
 		
 	}
 
