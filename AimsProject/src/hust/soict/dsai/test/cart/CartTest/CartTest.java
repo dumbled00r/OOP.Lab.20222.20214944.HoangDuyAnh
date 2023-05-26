54a0b4a191a2dc53c6dd76ea9884c05cd443fe21
dumbled00r <kazuu.promotion@gmail.com>
2023-05-26 15:24:19 +0700

    Update tests

--- AimsProject/src/hust/soict/dsai/test/cart/CartTest/CartTest.java
+++ AimsProject/src/hust/soict/dsai/test/cart/CartTest/CartTest.java
@@ -6,26 +6,27 @@ public class CartTest {
 		// TODO Auto-generated method stub
 		// Create a new cart
 		Cart cart = new Cart();
-		
 		// Create new dvd objects and add them to the cart
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
 				"Animation", "Roger Allers", 87, 19.95f);
-		
+		// add dvd to the cart
 		cart.addDigitalVideoDisc(dvd1);
-		
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
 				"Science Fiction", "George Lucas", 87, 24.95f);
+		// add dvd to the cart
 		cart.addDigitalVideoDisc(dvd2);
-		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Animation",
 				"Aladin", 18.99f);
-		
+		// add 2 same dvd to the cart to test
 		cart.addDigitalVideoDisc(dvd3);
 		cart.addDigitalVideoDisc(dvd3);
+		// print out cart
 		cart.print();
+		// cart's total cost
 		cart.totalCost();
+		// search for item by ID
 		cart.searchForItem(1);
-		
+		// search for item by title
+		cart.searchForItem("The Lion King");
 	}
-
 }
--- AimsProject/src/hust/soict/dsai/test/store/StoreTest/StoreTest.java
+++ AimsProject/src/hust/soict/dsai/test/store/StoreTest/StoreTest.java
@@ -1,36 +1,30 @@
 package hust.soict.dsai.test.store.StoreTest;
-
 import hust.soict.dsai.aims.store.Store.*;
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
 
 public class StoreTest {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Store store = new Store();
-		
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
 				"Animation", "Roger Allers", 87, 19.95f);
-		
+		// Add dvd to store
 		store.addDVD(dvd1);
 		
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", 
 				"Science Fiction", "George Lucas", 87, 24.95f);
-		
+		// add dvd to store
 		store.addDVD(dvd2);
 		
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Animation",
 				"Aladin", 18.99f);
-		
+		// add dvd to store
 		store.addDVD(dvd3);
-		
+		// Test print out store
 		store.print();
-		
+		// remove dvd from store
 		store.removeDVD(dvd3);
-		
+		// print out store after deleted
 		store.print();
-		
-		
 	}
-
 }
