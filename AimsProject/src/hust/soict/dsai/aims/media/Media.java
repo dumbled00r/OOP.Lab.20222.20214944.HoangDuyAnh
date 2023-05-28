956e5e6387ae31bf587a5b0fe59be360822620bd
dumbled00r <kazuu.promotion@gmail.com>
2023-05-28 12:53:41 +0700

    add comparators attributes

--- AimsProject/src/hust/soict/dsai/aims/media/Media.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Media.java
@@ -1,7 +1,11 @@
 package hust.soict.dsai.aims.media;
 
+import java.util.Comparator;
+
 public abstract class Media {
 	private static int nItems = 0;
+	public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost();
+	public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
 	private int id;
 	private String title;
 	private String category;
