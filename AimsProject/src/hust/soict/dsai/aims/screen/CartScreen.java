443d52cf0337516da92d51b3ae32e212b3d3f158
dumbled00r <kazuu.promotion@gmail.com>
2023-06-24 12:01:12 +0700

    Update cart screen

--- AimsProject/src/hust/soict/dsai/aims/screen/CartScreen.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/CartScreen.java
@@ -1,6 +1,9 @@
 package hust.soict.dsai.aims.screen;
 
 import hust.soict.dsai.aims.cart.Cart.Cart;
+import hust.soict.dsai.aims.media.Book;
+import hust.soict.dsai.aims.media.CompactDisc;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 
@@ -23,6 +27,7 @@ public class CartScreen extends JFrame {
         this.add(fxPanel);
 
         this.setTitle("Cart");
+        this.setSize(new Dimension(1024, 768));
         this.setVisible(true);
         Platform.runLater(new Runnable() {
             @Override
@@ -43,6 +48,49 @@ public class CartScreen extends JFrame {
     }
     public static void main(String[] args) {
         Cart cart = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Harry Porter",
+                "Magic", "JK Rowling", 127, 26.05f);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("IELTS Listening",
+                "English", "British Council", 40, 82.05f);
+
+        CompactDisc cd1 =
+                new CompactDisc("Making my way", "Pop",
+                        "Unknown", 5,  18.99f, "Son Tung M-TP");
+
+        CompactDisc cd2 =
+                new CompactDisc("Peaches", "Pop",
+                        "Unknown", 6,  30.99f, "Justin Bieber");
+
+        CompactDisc cd3 =
+                new CompactDisc("Kiss Me More", "Pop",
+                        "Unknown", 4,  30.99f, "Doja cat");
+
+        Book book1 =
+                new Book("Star Wars",
+                        "George Lucas", 24.95f);
+
+        Book book2 =
+                new Book("IELTS Reading",
+                        "IDP", 102.95f);
+
+        Book book3 =
+                new Book("How to make money",
+                        "Jack Ma", 69.95f);
+
+//         Add media to cart
+        cart.addMedia(book1);
+        cart.addMedia(book2);
+        cart.addMedia(book3);
+        cart.addMedia(cd1);
+        cart.addMedia(cd2);
+        cart.addMedia(cd3);
+        cart.addMedia(dvd1);
+        cart.addMedia(dvd2);
+        cart.addMedia(dvd3);
         new CartScreen(cart);
     }
 }
--- AimsProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
+++ AimsProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -9,46 +9,41 @@ import javafx.beans.value.ObservableValue;
 
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class CartScreenController {
     private Cart cart;
+    private double currentCost = 0;
+    private Runnable windowCloser;
 
     @FXML
     private javafx.scene.control.TableView<Media> tblMedia;
-
     @FXML
     private javafx.scene.control.TableColumn<Media, String> colMediaTitle;
-
     @FXML
     private javafx.scene.control.TableColumn<Media, String> colMediacategory;
-
     @FXML
     private javafx.scene.control.TableColumn<Media, Float> colMediaCost;
-
     @FXML
     private Button btnPlay;
-
     @FXML
     private Button btnRemove;
-
     @FXML
     private ToggleGroup filterCategory;
-
     @FXML
     private RadioButton radioBtnFilterId;
-
     @FXML
     private RadioButton radioBtnFilterTitle;
-
     @FXML
     private TextField tfFilter;
+    @FXML
+    private Button btnPlaceOrder;
+    @FXML
+    private Label lblCost;
 
     public CartScreenController(Cart cart) {
         super();
@@ -57,17 +52,25 @@ public class CartScreenController {
 
     @FXML
     private void initialize() {
+        // Set columns' headers
         colMediaTitle.setCellValueFactory(
                 new PropertyValueFactory<Media, String>("title"));
         colMediacategory.setCellValueFactory(
                 new PropertyValueFactory<Media, String>("category"));
         colMediaCost.setCellValueFactory(
                 new PropertyValueFactory<Media, Float>("cost"));
+        // Display current cart's items
         tblMedia.setItems(this.cart.getItemsOrdered());
 
+        // Set initial state of Play and Remove button
         btnPlay.setVisible(false);
-        btnPlay.setVisible(false);
+        btnRemove.setVisible(false);
 
+        // Cost and Total Cost
+        currentCost =this.cart.totalCost();
+        lblCost.setText(String.format("%.2f", currentCost) + "$");
+
+        // State of Play and Remove button
         tblMedia.getSelectionModel().selectedItemProperty().addListener(
                 new ChangeListener<Media>() {
                     @Override
@@ -81,11 +84,15 @@ public class CartScreenController {
                 }
         );
 
+        // remove button clicked
         btnRemove.setOnAction(e -> {
             Media media = tblMedia.getSelectionModel().getSelectedItem();
             cart.removeMedia(media);
+            currentCost -= media.getCost();
+            lblCost.setText(String.format("%.2f", currentCost) + "$");
         });
 
+        // Filtering actions
         tfFilter.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable,
@@ -94,8 +101,34 @@ public class CartScreenController {
                 showFilteredMedia(newValue);
             }
         });
+
+        // Place order button clicked
+        btnPlaceOrder.setOnAction(e -> {
+            cart.placeOrder();
+            lblCost.setText(Double.toString(cart.totalCost()) + "$");
+            btnPlay.setVisible(false);
+            btnRemove.setVisible(false);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Order placed");
+            alert.setHeaderText(null);
+            alert.setContentText("Your order has been placed!");
+            alert.showAndWait();
+        });
+
+        // Play button clicked
+        btnPlay.setOnAction(e -> {
+            Media media = tblMedia.getSelectionModel().getSelectedItem();
+            if (media instanceof Playable) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Playing...");
+                alert.setHeaderText(null);
+                alert.setContentText("The track is currently playing");
+                alert.showAndWait();
+            }
+        });
     }
 
+    // visibility of Play and remove button
     void updateButtonBar(Media media) {
         btnRemove.setVisible(true);
         if (media instanceof Playable) {
@@ -106,9 +139,18 @@ public class CartScreenController {
         }
     }
 
+    // Show filtered media
     void showFilteredMedia(String s) {
-
+        try {
+            if (!s.equals("")) {
+                if (radioBtnFilterId.isSelected()) {
+                    radioBtnFilterTitle.setSelected(false);
+                    tblMedia.setItems(cart.searchForItemByID(Integer.parseInt(s)));
+                } else if (radioBtnFilterTitle.isSelected()) {
+                    radioBtnFilterId.setSelected(false);
+                    tblMedia.setItems(cart.searchForItemByTitle(s));
+                }
+            } else tblMedia.setItems(cart.getItemsOrdered());
+        } catch (Exception e){}
     }
-
-
 }
